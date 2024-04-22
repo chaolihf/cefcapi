901dac2b6bd3d5d5f109c4232e7673c34b815027
chaoli <lichao01.ah@chinatelecom.cn>
2024-04-22 16:07:01 +0800

    start cef from go main process

--- main.go
+++ main.go
@@ -2,16 +2,26 @@ package main
 
 //#cgo CFLAGS: -I./cefapi
 //#cgo LDFLAGS: -L${SRCDIR}/cefapi -L${SRCDIR}/Release -lcefapi -lcef
-//
-//#include "cefapi.h"
+/*
+#include <stdio.h>
+#include "cefapi.h"
+*/
 import "C"
 
 //注意此处C需要和其他的包分开
 import (
 	"fmt"
+	"os"
+	"unsafe"
 )
 
 func main() {
-	fmt.Println("hello world")
 	fmt.Println(C.number_add_mod(10, 5, 12))
+	args := os.Args
+	argc := C.int(len(args))
+	argv := make([]*C.char, argc)
+	for i, arg := range args {
+		argv[i] = C.CString(arg)
+	}
+	C.startCef(argc, (**C.char)(unsafe.Pointer(&argv[0])))
 }
